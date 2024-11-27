b320b9d6f344513582d31566edb90b162316b1b5
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-27 13:15:31 +0000

    feat: add folders for all pages, finish navbar buttons

--- src/ui/layout/Navbar/NavbarButtons.tsx
+++ src/ui/layout/Navbar/NavbarButtons.tsx
@@ -5,11 +5,13 @@ import {
   ChartBarIcon,
 } from "@heroicons/react/24/outline";
 
+import Icon from "@/ui/shared/Icon";
+
 import { WrenchIcon } from "@heroicons/react/24/solid";
 
 const buttons = [
   { title: "Home", icon: HomeIcon, destination: "/" },
-  { title: "Track", icon: PresentationChartLineIcon, destination: "/track" },
+  { title: "Moods", icon: PresentationChartLineIcon, destination: "/moods" },
   { title: "Toolkit", icon: WrenchIcon, destination: "/toolkit" },
   { title: "Needs", icon: EllipsisHorizontalCircleIcon, destination: "/needs" },
   { title: "Insights", icon: ChartBarIcon, destination: "/insights" },
@@ -21,7 +23,7 @@ export default function NavbarButtons() {
       {buttons.map((button, index) => (
         <div key={index} className={`flex flex-col items-center`}>
           <p className="text-white">{button.title}</p>
-          <button.icon className="h-6 w-6 text-white" />
+          <Icon Icon={button.icon} classes="h-6 w-6 text-white" />
         </div>
       ))}
     </div>
--- src/ui/shared/Icon.tsx
+++ src/ui/shared/Icon.tsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+type IconProps = {
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  classes: string;
+};
+
+export default function Icon({ Icon, classes }: IconProps) {
+  return <Icon className={classes} />;
+}
