32cf7e016f6c8ebb3c56c8433a2d578d55f1f75b
maxitect <maxime.downe@gmail.com>
2024-11-27 18:28:46 +0000

    feat: plotly chart component for use to create 2D and 3D charts in nextjs

---
+++ src/ui/shared/PlotlyChart.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
+
+interface PlotlyChartProps {
+  data: Plotly.Data[];
+  layout: Partial<Plotly.Layout>;
+}
+
+export default function PlotlyChart({ data, layout }: PlotlyChartProps) {
+  return <Plot data={data} layout={layout} />;
+}
