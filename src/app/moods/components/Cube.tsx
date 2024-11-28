23ccf2059f846978e6eeea4fd42d884fef0ee5dd
maxitect <maxime.downe@gmail.com>
2024-11-28 14:10:47 +0000

    feat: 3d plotly chart working in Cube.tsx

--- src/app/moods/components/Cube.tsx
+++ src/app/moods/components/Cube.tsx
@@ -1,23 +1,89 @@
-'use client'
+"use client";
 
-import { useContext } from 'react'
-import { NeurochemContext } from '../page'
+import { useContext } from "react";
+import { NeurochemContext } from "../page";
+import PlotlyChart from "@/ui/shared/PlotlyChart";
+import { Datum } from "plotly.js";
 
 export function Cube() {
-  const context = useContext(NeurochemContext)
-  
+  const context = useContext(NeurochemContext);
+
   if (!context) {
-    throw new Error('Cube must be used within a NeurochemContext Provider')
+    throw new Error("Cube must be used within a NeurochemContext Provider");
   }
 
-  const { neuroState } = context
-  
+  const { neuroState } = context;
+
+  const renderValue = (value: Datum): string | number =>
+    value !== null && value instanceof Date
+      ? value.toISOString()
+      : value ?? "N/A";
+
   return (
-    <div className="p-4 border rounded">
-      <h3 className="text-white">Coordinates:</h3>
-      <p className="text-white">x (dopamine): {neuroState.dopamine}</p>
-      <p className="text-white">y (serotonin): {neuroState.serotonin}</p>
-      <p className="text-white">z (adrenaline): {neuroState.adrenaline}</p>
-    </div>
-  )
-} 
+    <>
+      <PlotlyChart
+        data={[
+          {
+            x: [neuroState.dopamine],
+            y: [neuroState.serotonin],
+            z: [neuroState.adrenaline],
+            type: "scatter3d",
+            mode: "markers",
+            marker: {
+              size: 5,
+              color: "#893FFC",
+              symbol: "circle",
+              line: {
+                color: "rgb(204, 204, 204)",
+                width: 1,
+              },
+            },
+          },
+        ]}
+        layout={{
+          width: 600,
+          height: 400,
+          title: { text: "Your mood" },
+          scene: {
+            xaxis: {
+              range: [0, 10], // Lock x-axis range
+              showticklabels: false, // Hide numbers
+              title: "", // Remove axis title
+              zeroline: true, // Show center zero line
+              gridcolor: "lightgray", // Set gridline color
+              nticks: 2, // Two ticks to create one half-line
+            },
+            yaxis: {
+              range: [0, 10], // Lock y-axis range
+              showticklabels: false, // Hide numbers
+              title: "", // Remove axis title
+              zeroline: true, // Show center zero line
+              gridcolor: "lightgray", // Set gridline color
+              nticks: 2, // Two ticks to create one half-line
+            },
+            zaxis: {
+              range: [0, 10], // Lock z-axis range
+              showticklabels: false, // Hide numbers
+              title: "", // Remove axis title
+              zeroline: true, // Show center zero line
+              gridcolor: "lightgray", // Set gridline color
+              nticks: 2, // Two ticks to create one half-line
+            },
+          },
+        }}
+      />
+      <div className="p-4 border rounded">
+        <h3 className="text-white">Coordinates:</h3>
+        <p className="text-white">
+          x (dopamine): {renderValue(neuroState.dopamine)}
+        </p>
+        <p className="text-white">
+          y (serotonin): {renderValue(neuroState.serotonin)}
+        </p>
+        <p className="text-white">
+          z (adrenaline): {renderValue(neuroState.adrenaline)}
+        </p>
+      </div>
+    </>
+  );
+}
