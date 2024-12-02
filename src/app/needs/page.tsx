fbd7790576aa64e6fdb3c0fa5f344872bf50aeeb
Jason Warren <contact.jwarren@gmail.com>
2024-12-02 16:53:21 +0000

    refactor(page): remove database destruction functionality and associated button

--- src/app/needs/page.tsx
+++ src/app/needs/page.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { destroyDatabase } from "@/lib/db/rxdbInit";
-import Button from "@/ui/shared/Button";
-
 export default function Page() {
-  const handleDestroyDB = async () => {
-    try {
-      await destroyDatabase();
-      console.log("Database successfully destroyed");
-    } catch (error) {
-      console.error("Error destroying database:", error);
-    }
-  };
-
   return (
     <>
       <h1 className="text-white">this is the needs page</h1>
-      <Button
-        label="Destroy Database"
-        onClick={handleDestroyDB}
-        className="bg-red-500 hover:bg-red-700 text-white"
-      />
     </>
   );
 }
