4b283acd8ac68323834f53157b2a9bc5551553fe
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-02 14:46:08 +0000

    feat: add buttons for moods, no functionality

--- src/app/moods/page.tsx
+++ src/app/moods/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Cube } from "./components/Cube";
 import { SliderBox } from "./components/SliderBox";
 import { NeurochemContext, NeurochemState } from "@/context/NeurochemContext";
+import Button from "@/ui/shared/Button";
 
 export default function MoodsPage() {
   const [neuroState, setNeuroState] = useState<NeurochemState>({
@@ -17,6 +18,16 @@ export default function MoodsPage() {
       <div className="flex flex-col gap-4">
         <Cube />
         <SliderBox />
+        <div className="flex justify-between w-10/12 m-auto">
+          <Button
+            label="Save and Exit"
+            className="border-white border-2 border-solid"
+          />
+          <Button
+            label="Continue to Toolkit"
+            className="bg-twd-primary-purple"
+          />
+        </div>
       </div>
     </NeurochemContext.Provider>
   );
