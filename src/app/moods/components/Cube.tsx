441bd201ba0be1f62414e455f8e555169953cd9f
maxitect <maxime.downe@gmail.com>
2024-12-03 16:04:57 +0000

    feat: attach legends to Cube.tsx, simplify the aesthetic

--- src/app/moods/components/Cube.tsx
+++ src/app/moods/components/Cube.tsx
@@ -43,9 +43,9 @@ export function Cube() {
     {
       // Bottom-front-left
       type: "mesh3d",
-      x: [0, 5, 5, 0, 0, 5, 5, 0],
-      y: [0, 0, 5, 5, 0, 0, 5, 5],
-      z: [0, 0, 0, 0, 5, 5, 5, 5],
+      x: [0, 5.5, 5.5, 0, 0, 5.5, 5.5, 0],
+      y: [0, 0, 5.5, 5.5, 0, 0, 5.5, 5.5],
+      z: [0, 0, 0, 0, 5.5, 5.5, 5.5, 5.5],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -59,9 +59,9 @@ export function Cube() {
     {
       // Bottom-front-right
       type: "mesh3d",
-      x: [5, 10, 10, 5, 5, 10, 10, 5],
-      y: [0, 0, 5, 5, 0, 0, 5, 5],
-      z: [0, 0, 0, 0, 5, 5, 5, 5],
+      x: [5.5, 11, 11, 5.5, 5.5, 11, 11, 5.5],
+      y: [0, 0, 5.5, 5.5, 0, 0, 5.5, 5.5],
+      z: [0, 0, 0, 0, 5.5, 5.5, 5.5, 5.5],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -75,9 +75,9 @@ export function Cube() {
     {
       // Bottom-back-left
       type: "mesh3d",
-      x: [0, 5, 5, 0, 0, 5, 5, 0],
-      y: [5, 5, 10, 10, 5, 5, 10, 10],
-      z: [0, 0, 0, 0, 5, 5, 5, 5],
+      x: [0, 5.5, 5.5, 0, 0, 5.5, 5.5, 0],
+      y: [5.5, 5.5, 11, 11, 5.5, 5.5, 11, 11],
+      z: [0, 0, 0, 0, 5.5, 5.5, 5.5, 5.5],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -91,9 +91,9 @@ export function Cube() {
     {
       // Bottom-back-right
       type: "mesh3d",
-      x: [5, 10, 10, 5, 5, 10, 10, 5],
-      y: [5, 5, 10, 10, 5, 5, 10, 10],
-      z: [0, 0, 0, 0, 5, 5, 5, 5],
+      x: [5.5, 11, 11, 5.5, 5.5, 11, 11, 5.5],
+      y: [5.5, 5.5, 11, 11, 5.5, 5.5, 11, 11],
+      z: [0, 0, 0, 0, 5.5, 5.5, 5.5, 5.5],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -107,9 +107,9 @@ export function Cube() {
     {
       // Top-front-left
       type: "mesh3d",
-      x: [0, 5, 5, 0, 0, 5, 5, 0],
-      y: [0, 0, 5, 5, 0, 0, 5, 5],
-      z: [5, 5, 5, 5, 10, 10, 10, 10],
+      x: [0, 5.5, 5.5, 0, 0, 5.5, 5.5, 0],
+      y: [0, 0, 5.5, 5.5, 0, 0, 5.5, 5.5],
+      z: [5.5, 5.5, 5.5, 5.5, 11, 11, 11, 11],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -123,9 +123,9 @@ export function Cube() {
     {
       // Top-front-right
       type: "mesh3d",
-      x: [5, 10, 10, 5, 5, 10, 10, 5],
-      y: [0, 0, 5, 5, 0, 0, 5, 5],
-      z: [5, 5, 5, 5, 10, 10, 10, 10],
+      x: [5.5, 11, 11, 5.5, 5.5, 11, 11, 5.5],
+      y: [0, 0, 5.5, 5.5, 0, 0, 5.5, 5.5],
+      z: [5.5, 5.5, 5.5, 5.5, 11, 11, 11, 11],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -139,9 +139,9 @@ export function Cube() {
     {
       // Top-back-left
       type: "mesh3d",
-      x: [0, 5, 5, 0, 0, 5, 5, 0],
-      y: [5, 5, 10, 10, 5, 5, 10, 10],
-      z: [5, 5, 5, 5, 10, 10, 10, 10],
+      x: [0, 5.5, 5.5, 0, 0, 5.5, 5.5, 0],
+      y: [5.5, 5.5, 11, 11, 5.5, 5.5, 11, 11],
+      z: [5.5, 5.5, 5.5, 5.5, 11, 11, 11, 11],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -155,9 +155,9 @@ export function Cube() {
     {
       // Top-back-right
       type: "mesh3d",
-      x: [5, 10, 10, 5, 5, 10, 10, 5],
-      y: [5, 5, 10, 10, 5, 5, 10, 10],
-      z: [5, 5, 5, 5, 10, 10, 10, 10],
+      x: [5.5, 11, 11, 5.5, 5.5, 11, 11, 5.5],
+      y: [5.5, 5.5, 11, 11, 5.5, 5.5, 11, 11],
+      z: [5.5, 5.5, 5.5, 5.5, 11, 11, 11, 11],
       alphahull: 1,
       flatshading: true,
       opacity: 0.3,
@@ -188,7 +188,7 @@ export function Cube() {
     {
       // Bottom-front-right
       type: "scatter3d",
-      x: [10],
+      x: [11],
       y: [0],
       z: [1],
       text: ["Relief"],
@@ -202,9 +202,9 @@ export function Cube() {
     {
       // Bottom-back-left
       type: "scatter3d",
-      x: [2.5],
-      y: [5],
-      z: [2.5],
+      x: [2.75],
+      y: [5.5],
+      z: [2.75],
       text: ["Guilt"],
       mode: "text",
       textposition: "middle center",
@@ -216,9 +216,9 @@ export function Cube() {
     {
       // Bottom-back-right
       type: "scatter3d",
-      x: [7.5],
-      y: [5],
-      z: [2.5],
+      x: [7.75],
+      y: [5.5],
+      z: [2.75],
       text: ["Content"],
       mode: "text",
       textposition: "middle center",
@@ -232,7 +232,7 @@ export function Cube() {
       type: "scatter3d",
       x: [0],
       y: [0],
-      z: [9],
+      z: [10],
       text: ["Fight/flight"],
       mode: "text",
       textposition: "middle right",
@@ -244,9 +244,9 @@ export function Cube() {
     {
       // Top-front-right
       type: "scatter3d",
-      x: [10],
+      x: [11],
       y: [0],
-      z: [9],
+      z: [10],
       text: ["Interest"],
       mode: "text",
       textposition: "middle left",
@@ -258,9 +258,9 @@ export function Cube() {
     {
       // Top-back-left
       type: "scatter3d",
-      x: [2.5],
-      y: [5],
-      z: [7.5],
+      x: [2.75],
+      y: [5.5],
+      z: [7.75],
       text: ["Freeze"],
       mode: "text",
       textposition: "middle center",
@@ -272,9 +272,9 @@ export function Cube() {
     {
       // Top-back-right
       type: "scatter3d",
-      x: [7.5],
-      y: [5],
-      z: [7.5],
+      x: [7.75],
+      y: [5.5],
+      z: [7.75],
       text: ["Joy"],
       mode: "text",
       textposition: "middle center",
@@ -286,72 +286,95 @@ export function Cube() {
   ];
 
   return (
-    <div className="flex justify-center">
-      <PlotlyChart
-        data={[
-          {
-            x: [neuroState.dopamine ?? 0],
-            y: [10 - (Number(neuroState.serotonin) ?? 0)],
-            z: [neuroState.adrenaline ?? 0],
-            type: "scatter3d",
-            mode: "markers",
-            marker: {
-              size: 12,
-              color: "#000000",
-              opacity: 1,
-              symbol: "circle",
-              line: {
-                color: "rgb(204, 204, 204)",
-                width: 1,
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="relative flex justify-center w-full">
+        {/* Left side labels - positioned absolutely */}
+        <div className="absolute left-0 top-1/2 transform -translate-y-1/2 flex flex-col justify-between h-[200px]">
+          <span className="rotate-[-90deg] origin-center whitespace-nowrap text-white text-sm">
+            Urgent
+          </span>
+          <span className="rotate-[-90deg] origin-center whitespace-nowrap text-white text-sm">
+            Not urgent
+          </span>
+        </div>
+
+        {/* Plotly Chart - centered */}
+        <div className="flex justify-center h-[250px] w-[250px]">
+          <PlotlyChart
+            data={[
+              {
+                x: [neuroState.dopamine ?? 0],
+                y: [10 - (Number(neuroState.serotonin) ?? 0)],
+                z: [neuroState.adrenaline ?? 0],
+                type: "scatter3d",
+                mode: "markers",
+                marker: {
+                  size: 6,
+                  color: "#fff",
+                  opacity: 1,
+                  symbol: "circle",
+                  line: {
+                    color: "rgb(204, 204, 204)",
+                    width: 1,
+                  },
+                },
               },
-            },
-          },
-          ...cubeLabels,
-          ...cubeMeshes,
-        ]}
-        layout={{
-          paper_bgcolor: "#1B192E",
-          hidesources: true,
-          width: 250,
-          height: 250,
-          margin: {
-            l: 0,
-            r: 0,
-            t: 20,
-            b: 0,
-            pad: 0,
-          },
-          showlegend: false,
-          scene: {
-            aspectmode: "cube",
-            dragmode: false,
-            camera: {
-              eye: {
-                x: 0,
-                y: -1.7,
-                z: 0,
+              ...cubeLabels,
+              ...cubeMeshes,
+            ]}
+            layout={{
+              paper_bgcolor: "#1B192E",
+              hidesources: true,
+              width: 250,
+              height: 250,
+              hovermode: false,
+              clickmode: "none",
+              dragmode: false,
+              margin: {
+                l: 0,
+                r: 0,
+                t: 5,
+                b: 0,
+                pad: 0,
               },
-            },
-            xaxis: {
-              range: [0, 10],
-              showticklabels: false,
-            },
-            yaxis: {
-              range: [0, 10],
-              showticklabels: false,
-            },
-            zaxis: {
-              range: [0, 10],
-              showticklabels: false,
-            },
-          },
-        }}
-        config={{
-          modeBarButtons: false,
-          displaylogo: false,
-          staticPlot: true,
-        }}
-      />
+              showlegend: false,
+              scene: {
+                aspectmode: "cube",
+                dragmode: false,
+                camera: {
+                  eye: {
+                    x: 0,
+                    y: -1.7,
+                    z: 0,
+                  },
+                },
+                xaxis: {
+                  range: [0, 11],
+                  showticklabels: false,
+                },
+                yaxis: {
+                  range: [0, 11],
+                  showticklabels: false,
+                },
+                zaxis: {
+                  range: [0, 11],
+                  showticklabels: false,
+                },
+              },
+            }}
+            config={{
+              displayModeBar: false,
+              staticPlot: true,
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Bottom labels - centered with chart */}
+      <div className="flex justify-between w-[250px] mt-4">
+        <span className="text-white text-sm">Not important</span>
+        <span className="text-white text-sm">Important</span>
+      </div>
     </div>
   );
 }
