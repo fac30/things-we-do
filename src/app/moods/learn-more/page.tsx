abd4497b28ea9742a1ec462b73d3f2b1616f4755
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-03 09:25:34 +0000

    chore: committing to merge

---
+++ src/app/moods/learn-more/page.tsx
@@ -0,0 +1,23 @@
+import cubeImage from "../../../assets/images/cubeImage.png";
+import Image from "next/image";
+
+export default function LearnMorePageMoods() {
+  return (
+    <>
+      <h1>Learn More</h1>
+      <div className="">toggle</div>
+      <h2>
+        Seeing how urgent vs how important tasks are can help you prioritise
+        them
+      </h2>
+      <div className="w-11/12 m-auto flex justify-center mt-10">
+        <Image
+          src={cubeImage}
+          width={250}
+          height={250}x
+          alt="Picture of the author"
+        />
+      </div>
+    </>
+  );
+}
