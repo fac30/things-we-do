9ffced2e546591e923b8a88f36e1ffbf2b402d8c
maxitect <maxime.downe@gmail.com>
2024-12-03 16:04:28 +0000

    chore: semantic html changes to SliderBox.tsx to wrap inside form/fieldset

--- src/app/moods/components/SliderBox.tsx
+++ src/app/moods/components/SliderBox.tsx
@@ -4,13 +4,18 @@ import { Slider } from "./Slider";
 
 export function SliderBox() {
   return (
-    <div className="flex flex-col gap-4 w-10/12 m-auto">
-      <h2 className="text-xl text-center mb-4 font-semiboldbold">
-        How does this task feel right now?
-      </h2>
-      <Slider chem="dopamine" label="Step 1. How urgent does it feel?" />
-      <Slider chem="serotonin" label="Step 2. How much effort does it take?" />
-      <Slider chem="adrenaline" label="Step 3. Does it feel worth doing?" />
-    </div>
+    <form className="flex flex-col gap-4 w-10/12 m-auto">
+      <fieldset>
+        <legend className="m-auto text-xl text-center mb-3 font-semiboldbold">
+          How does this task feel right now?
+        </legend>
+        <Slider chem="dopamine" label="Step 1. How urgent does it feel?" />
+        <Slider
+          chem="serotonin"
+          label="Step 2. How much effort does it take?"
+        />
+        <Slider chem="adrenaline" label="Step 3. Does it feel worth doing?" />
+      </fieldset>
+    </form>
   );
 }
