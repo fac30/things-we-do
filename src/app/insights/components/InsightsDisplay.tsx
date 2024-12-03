5a21082486afa8e174707bad7752da46f5a97553
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-03 14:00:15 +0000

    feat: display neurotransmitters on insights page

--- src/app/insights/components/InsightsDisplay.tsx
+++ src/app/insights/components/InsightsDisplay.tsx
@@ -2,24 +2,33 @@
 
 import { getFromDb } from "@/models/getFromDb";
 import { useEffect, useState } from "react";
-import { getSingleDocument } from "rxdb";
 
 export default function InsightsDisplay() {
-  const [insights, setInsights] = useState();
+  const [insights, setInsights] = useState([]);
 
   const getInsights = async () => {
     const myInsights = await getFromDb();
+    console.log(myInsights);
     setInsights(myInsights);
   };
 
   useEffect(() => {
     getInsights();
-  });
+  }, []);
 
   return (
     <>
-      <h1>he</h1>
-      <h1>llo</h1>
+      {insights.length > 0 ? (
+        insights.map((insight, index) => (
+          <div key={index} className="mb-5">
+            <h1>dopamine: {insight._data.neurotransmitters.dopamine}</h1>
+            <h1>serotonin: {insight._data.neurotransmitters.serotonin}</h1>
+            <h1>adrenaline: {insight._data.neurotransmitters.adrenaline}</h1>
+          </div>
+        ))
+      ) : (
+        <h1>No insights available</h1> // Fallback message when no insights are available
+      )}
     </>
   );
 }
--- src/models/getFromDb.ts
+++ src/models/getFromDb.ts
@@ -3,6 +3,7 @@ import rxdbInit from "@/lib/db/rxdbInit";
 export async function getFromDb() {
   const db = await rxdbInit();
 
-  const myCollection = db.mood_records;
-  console.log(myCollection);
+  const myCollection = await db.mood_records.find().exec();
+  console.log(myCollection[0]._data);
+  return myCollection;
 }
