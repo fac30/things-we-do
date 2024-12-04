0d282a2926ab94001e831080566c9dc08eb8db65
Jason Warren <contact.jwarren@gmail.com>
2024-12-04 17:56:44 +0000

    refactor: reconcile tanya & jason's DB methods
    - Added renamed `insertToDb` to `seedToolkitItems`

--- src/lib/db/DatabaseManager.ts
+++ src/lib/db/DatabaseManager.ts
@@ -70,6 +70,32 @@ class DatabaseManager {
     }
     return this.dbInstance;
   }
+  
+  async seedToolkitItems() {
+    try {
+      const db = await this.initialiseDatabase();
+      if (!db) {
+        console.error("Database initialisation failed");
+        return;
+      }
+      const toolkitCollection = db.toolkit_items;
+      if (!toolkitCollection) {
+        console.error("Toolkit items collection does not exist");
+        return;
+      }
+      console.log("Checking if toolkit_items collection is already seeded...");
+      const existingDocuments = await toolkitCollection.find().exec();
+      if (existingDocuments.length > 0) {
+            console.log("Toolkit items collection is already seeded. Skipping seeding process.");
+            return;
+        }
+      console.log("Seeding toolkit_items collection with initial data...");
+      const insertedDocs = await toolkitCollection.bulkInsert(toolkitSeedData);
+      console.log("Seed data successfully inserted:", insertedDocs);
+    } catch (error) {
+        console.error("Error seeding the database:", error);
+    }
+  }
 
   async getFromDb(collection: string) {
     const db = await this.initialiseDatabase();
@@ -124,33 +150,7 @@ class DatabaseManager {
       console.error("Error adding document to database:", error);
     }
   }
-
-  async seedToolkitItems() {
-    try {
-      const db = await this.initialiseDatabase();
-      if (!db) {
-        console.error("Database initialisation failed");
-        return;
-      }
-      const toolkitCollection = db.toolkit_items;
-      if (!toolkitCollection) {
-        console.error("Toolkit items collection does not exist");
-        return;
-      }
-      console.log("Checking if toolkit_items collection is already seeded...");
-      const existingDocuments = await toolkitCollection.find().exec();
-      if (existingDocuments.length > 0) {
-            console.log("Toolkit items collection is already seeded. Skipping seeding process.");
-            return;
-        }
-      console.log("Seeding toolkit_items collection with initial data...");
-      const insertedDocs = await toolkitCollection.bulkInsert(toolkitSeedData);
-      console.log("Seed data successfully inserted:", insertedDocs);
-    } catch (error) {
-        console.error("Error seeding the database:", error);
-    }
-  }
-
+  
   async addCategory(name: string) {
     try {
       const doc = {
