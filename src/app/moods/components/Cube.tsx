7f067e67b28950136bd491f594df29635339ebcc
maxitect <maxime.downe@gmail.com>
2024-12-04 13:10:19 +0000

    refactor: move cube labels data to json file outside of Cube.tsx

--- src/app/moods/components/Cube.tsx
+++ src/app/moods/components/Cube.tsx
@@ -4,9 +4,10 @@ import { useContext } from "react";
 import { NeurochemContext } from "@/context/NeurochemContext";
 import PlotlyChart from "@/ui/shared/PlotlyChart";
 import { PlotData } from "plotly.js";
-import quadrants from "./data/quadrants.json";
 
-// Cube quadrants order (in quadrants.json):
+import quadrants from "./data/quadrants.json";
+import labels from "./data/labels.json";
+// Cube quadrants and labels order (in quadrants.json/labels.json):
 // Bottom-front-left
 // Bottom-front-right
 // Bottom-back-left
@@ -50,121 +51,6 @@ export function Cube() {
 
   const { neuroState } = context;
 
-  const cubeLabels: TextLabel[] = [
-    {
-      // Bottom-front-left
-      type: "scatter3d",
-      x: [0],
-      y: [0],
-      z: [1],
-      text: ["Distress"],
-      mode: "text",
-      textposition: "middle right",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Bottom-front-right
-      type: "scatter3d",
-      x: [11],
-      y: [0],
-      z: [1],
-      text: ["Relief"],
-      mode: "text",
-      textposition: "middle left",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Bottom-back-left
-      type: "scatter3d",
-      x: [2.75],
-      y: [5.5],
-      z: [2.75],
-      text: ["Guilt"],
-      mode: "text",
-      textposition: "middle center",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Bottom-back-right
-      type: "scatter3d",
-      x: [7.75],
-      y: [5.5],
-      z: [2.75],
-      text: ["Content"],
-      mode: "text",
-      textposition: "middle center",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Top-front-left
-      type: "scatter3d",
-      x: [0],
-      y: [0],
-      z: [10],
-      text: ["Fight/flight"],
-      mode: "text",
-      textposition: "middle right",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Top-front-right
-      type: "scatter3d",
-      x: [11],
-      y: [0],
-      z: [10],
-      text: ["Interest"],
-      mode: "text",
-      textposition: "middle left",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Top-back-left
-      type: "scatter3d",
-      x: [2.75],
-      y: [5.5],
-      z: [7.75],
-      text: ["Freeze"],
-      mode: "text",
-      textposition: "middle center",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-    {
-      // Top-back-right
-      type: "scatter3d",
-      x: [7.75],
-      y: [5.5],
-      z: [7.75],
-      text: ["Joy"],
-      mode: "text",
-      textposition: "middle center",
-      textfont: {
-        size: 15,
-        color: "white",
-      },
-    },
-  ];
-
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div className="relative flex justify-center w-full">
@@ -199,7 +85,7 @@ export function Cube() {
                   },
                 },
               },
-              ...cubeLabels,
+              ...(labels as TextLabel[]),
               ...(quadrants as Partial<ExtendedMesh3d>[]),
             ]}
             layout={{
