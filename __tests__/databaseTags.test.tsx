303f019df5f222338375cb434b8773c0d9549f7b
Jason Warren <contact.jwarren@gmail.com>
2024-12-04 17:19:10 +0000

    test: remove obsolete category database tests
    - Deleted placeholder `databaseTags.test.tsx`

--- __tests__/databaseTags.test.tsx
+++
@@ -1,9 +0,0 @@
-describe("Category Database Operations", () => {
-  it("adds new category to database", async () => {
-    // Test category insertion
-  });
-
-  it("fetches categories from database", async () => {
-    // Test category retrieval
-  });
-});
