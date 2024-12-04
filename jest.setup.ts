9c94429ab727702041b2c225f4248258b7b53c3e
Jason Warren <contact.jwarren@gmail.com>
2024-12-04 14:50:43 +0000

    feat: :sparkles: add window.confirm and mock crypto.randomUUID for testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,3 +4,14 @@ Object.defineProperty(window, 'alert', {
   writable: true,
   value: jest.fn(),
 });
+
+Object.defineProperty(window, 'confirm', {
+  writable: true,
+  value: jest.fn(),
+});
+
+// Mock crypto.randomUUID
+Object.defineProperty(global.crypto, 'randomUUID', {
+  writable: true,
+  value: jest.fn(() => '123e4567-e89b-12d3-a456-426614174000'),
+});
