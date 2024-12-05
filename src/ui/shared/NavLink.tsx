e6334f240b6ace21f61ae6f462cee1eac53e539d
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-05 14:21:31 +0000

    style: add shadow to line above active navbar link

--- src/ui/shared/NavLink.tsx
+++ src/ui/shared/NavLink.tsx
@@ -24,7 +24,7 @@ export default function NavLink({
     <Link
       href={destination}
       className={clsx(
-        `flex flex-col items-center justify-center w-14 h-14 text-gray-400 relative`, // add 'relative' for positioning the pseudo-element
+        `flex flex-col items-center justify-center w-14 h-14 text-gray-400 relative`,
         {
           "text-white": isActive,
           "border-twd-primary-purple border-solid border-[3px] rounded-full p-10 bg-twd-navbar-background shadow-[0px_0px_8px_2px_rgba(137,63,252)] -translate-y-4":
@@ -35,7 +35,7 @@ export default function NavLink({
     >
       {isActive && !isToolkit && (
         <span
-          className="absolute top-[-9%] left-0 w-full h-[2.5px] bg-white "
+          className="absolute top-[-5.5px] left-0 w-full h-[2.5px] bg-white "
           style={{ boxShadow: "0 0 15px rgba(255, 255, 255, 0.5)" }}
         />
       )}
