1ce825b4c01946d8a361fa4d99f171abfa935c16
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-05 10:50:14 +0000

    chore: fix type errors in retrieve data object

--- src/lib/utils/retrieveDataObject.ts
+++ src/lib/utils/retrieveDataObject.ts
@@ -1,3 +1,6 @@
-export default function retrieveDataObject(dataArr) {
+import { RxDocument } from "rxdb";
+import { Insight } from "@/app/insights/components/InsightsDisplay";
+
+export default function retrieveDataObject(dataArr: RxDocument<Insight>[]) {
   return dataArr.map((item) => item._data);
 }
