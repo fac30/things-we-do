1c90b343506468400e17119ec4b4f811f04ad040
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-06 23:21:13 +0000

    chore: remove useclient from slider and sliderbox

--- src/app/moods/components/Slider.tsx
+++ src/app/moods/components/Slider.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Datum } from "plotly.js";
 
 interface SliderProps {
--- src/app/moods/components/SliderBox.tsx
+++ src/app/moods/components/SliderBox.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Slider } from "./Slider";
 import { Datum } from "plotly.js";
 
