a933b98c6eeea37a7d5cd5796ca74d3a3dae61db
Jason Warren <contact.jwarren@gmail.com>
2024-12-07 12:54:39 +0000

    feat(setup): :card_file_box: seed categories

--- src/lib/db/DatabaseManager.ts
+++ src/lib/db/DatabaseManager.ts
@@ -9,84 +9,159 @@ import { v4 as uuidv4 } from "uuid";
 
 addRxPlugin(RxDBDevModePlugin);
 
-const toolkitSeedData = [
-  {
-    id: uuidv4(),
-    name: "Listen to my favourite music",
-    categories: ["Replace", "Barrier"],
-    checked: false,
-    infoUrl: "https://google.com/music",
-    imageUrl:
-      "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
-    timestamp: new Date().toISOString(),
-  },
-  {
-    id: uuidv4(),
-    name: "Watch TV",
-    categories: ["Distract"],
-    checked: false,
-    infoUrl: "https://google.com/tv",
-    imageUrl:
-      "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
-    timestamp: new Date().toISOString(),
-  },
-  {
-    id: uuidv4(),
-    name: "Call a friend",
-    categories: ["Distract", "Change status"],
-    checked: false,
-    infoUrl: "https://example.com/call",
-    imageUrl:
-      "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
-    timestamp: new Date().toISOString(),
-  },
-  {
-    id: uuidv4(),
-    name: "See a friend",
-    categories: ["Distract", "Change status"],
-    checked: false,
-    infoUrl: "https://example.com/call",
-    imageUrl:
-      "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
-    timestamp: new Date().toISOString(),
-  },
-];
+const seedData = {
+  categories: [
+    {
+      id: uuidv4(),
+      name: "Replace",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "Barrier",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "Distract",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "Change Status",
+      timestamp: new Date().toISOString(),
+    },
+  ],
+  toolkit: [
+    {
+      id: uuidv4(),
+      name: "Listen to my favourite music",
+      categories: ["Replace", "Barrier"],
+      checked: false,
+      infoUrl: "https://google.com/music",
+      imageUrl:
+        "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "Watch TV",
+      categories: ["Distract"],
+      checked: false,
+      infoUrl: "https://google.com/tv",
+      imageUrl:
+        "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "Call a friend",
+      categories: ["Distract", "Change status"],
+      checked: false,
+      infoUrl: "https://example.com/call",
+      imageUrl:
+        "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
+      timestamp: new Date().toISOString(),
+    },
+    {
+      id: uuidv4(),
+      name: "See a friend",
+      categories: ["Distract", "Change status"],
+      checked: false,
+      infoUrl: "https://example.com/call",
+      imageUrl:
+        "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg",
+      timestamp: new Date().toISOString(),
+    },
+  ],
+};
 
 class DatabaseManager {
   dbInstance: RxDatabase | null = null;
 
   async initialiseDatabase() {
+    console.groupCollapsed("Initialising Database");
     if (!this.dbInstance) {
+      console.groupCollapsed("Creating New Database");
       this.dbInstance = await createRxDatabase({
         name: "database",
         //ignoreDuplicate: true,
         storage: getRxStorageDexie(),
       });
+      console.log("Database Created");
+      console.groupEnd();
 
+      console.groupCollapsed("Adding Collections");
       await this.dbInstance.addCollections({
         categories: { schema: categoriesSchema },
         mood_records: { schema: moodRecordSchema },
         toolkit_items: { schema: toolkitItemSchema },
       });
+      console.log("Collections Added");
+      console.groupEnd();
 
-      console.log("Database initialised");
-      await this.seedToolkitItems(); // Automatically seed data
+      console.groupCollapsed("Seeding Database");
+      await this.seedCategories();
+      await this.seedToolkitItems();
+      console.log("Database Seeded");
+      console.groupEnd();
     }
+    console.groupEnd();
     return this.dbInstance;
   }
-
-  async seedToolkitItems() {
+  
+  async seedCategories() {
+    console.groupCollapsed("Seeding Categories");
     try {
+      console.log("Initialising Database");
       const db = await this.initialiseDatabase();
       if (!db) {
         console.error("Database initialisation failed");
         return;
       }
+
+      console.log("Looking for categories collection");
+      const categoriesCollection = db.categories;
+      if (!categoriesCollection) {
+        console.error("Categories collection does not exist");
+        return;
+      }
+
+      console.log("Checking if categories collection is already seeded...");
+      const existingDocuments = await categoriesCollection.find().exec();
+      if (existingDocuments.length > 0) {
+        console.log(
+          "Categories collection is already seeded. Skipping seeding process."
+        );
+        return;
+      }
+
+      console.log("Seeding categories collection with initial data...");
+      const insertedDocs = await categoriesCollection.bulkInsert(seedData.categories);
+      console.log("Categories Seeded:", insertedDocs);
+    } catch (error) {
+      console.error("Error seeding the categories:", error);
+    }
+    console.groupEnd();
+  }
+
+  async seedToolkitItems() {
+    console.groupCollapsed("Seeding Toolkit Items");
+    try {
+      console.log("Initialising Database");
+      const db = await this.initialiseDatabase();
+      if (!db) {
+        console.error("Database initialisation failed");
+        return;
+      }
+
+      console.log("Looking for toolkit_items collection");
       const toolkitCollection = db.toolkit_items;
       if (!toolkitCollection) {
         console.error("Toolkit items collection does not exist");
         return;
       }
+
       console.log("Checking if toolkit_items collection is already seeded...");
       const existingDocuments = await toolkitCollection.find().exec();
       if (existingDocuments.length > 0) {
@@ -95,12 +170,14 @@ class DatabaseManager {
         );
         return;
       }
+
       console.log("Seeding toolkit_items collection with initial data...");
-      const insertedDocs = await toolkitCollection.bulkInsert(toolkitSeedData);
-      console.log("Seed data successfully inserted:", insertedDocs);
+      const insertedDocs = await toolkitCollection.bulkInsert(seedData.toolkit);
+      console.log("Toolkit Items Seeded:", insertedDocs);
     } catch (error) {
       console.error("Error seeding the database:", error);
     }
+    console.groupEnd();
   }
 
   async getFromDb(collection: string) {
