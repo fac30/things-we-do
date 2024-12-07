b4d7abd8deba6de6bf4021e61e9d3a50b546f31e
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-07 10:39:55 +0000

    refactor: use a .map for sliders to keep code DRY

--- src/app/moods/components/SliderBox.tsx
+++ src/app/moods/components/SliderBox.tsx
@@ -9,6 +9,15 @@ interface SliderBoxProps {
   neuroState: NeurochemState;
 }
 
+const sliders: {
+  chem: "dopamine" | "serotonin" | "adrenaline";
+  label: string;
+}[] = [
+  { chem: "dopamine", label: "Step 1. How urgent does it feel?" },
+  { chem: "serotonin", label: "Step 2. How much effort does it take?" },
+  { chem: "adrenaline", label: "Step 3. Does it feel worth doing?" },
+];
+
 export function SliderBox({ handleChange, neuroState }: SliderBoxProps) {
   return (
     <form className="flex flex-col gap-4 w-10/12 m-auto">
@@ -16,24 +25,18 @@ export function SliderBox({ handleChange, neuroState }: SliderBoxProps) {
         <legend className="m-auto text-xl text-center mb-3 font-semiboldbold">
           How does this task feel right now?
         </legend>
-        <Slider
-          chem="dopamine"
-          label="Step 1. How urgent does it feel?"
-          handleChange={handleChange}
-          neuroState={neuroState}
-        />
-        <Slider
-          chem="serotonin"
-          label="Step 2. How much effort does it take?"
-          handleChange={handleChange}
-          neuroState={neuroState}
-        />
-        <Slider
-          chem="adrenaline"
-          label="Step 3. Does it feel worth doing?"
-          handleChange={handleChange}
-          neuroState={neuroState}
-        />
+        {sliders.map((slider) => {
+          return (
+            <>
+              <Slider
+                chem={slider.chem}
+                label={slider.label}
+                handleChange={handleChange}
+                neuroState={neuroState}
+              />
+            </>
+          );
+        })}
       </fieldset>
     </form>
   );
