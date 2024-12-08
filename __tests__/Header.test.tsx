9fa0cf3bbbe388b723e461e462d75135b6f841d6
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-08 12:33:17 +0000

    chore: add tests

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -1,37 +1,77 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Header } from "@/ui/shared/Header";
+import { useRouter } from "next/navigation";
 
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
 }));
 
 describe("Header", () => {
+  const mockedBack = jest.fn();
+
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue({ back: mockedBack });
+  });
+
   it("renders the title and description", () => {
     render(
       <Header
         title="Test Title"
         description="Test Description"
-        isHome={false}
+        hasInfoButton={false}
+        isInfoPage={false}
       />
     );
     expect(screen.getByText("Test Title")).toBeInTheDocument();
   });
 
-  it("does not render the InfoButton when isHome is true", () => {
-    render(
-      <Header title="Test Title" description="Test Description" isHome={true} />
-    );
-    expect(screen.queryByRole("button")).not.toBeInTheDocument();
-  });
-
-  it("renders the InfoButton when isHome is false", () => {
+  it("does not render the InfoButton when hasInfoButton is false", () => {
     render(
       <Header
         title="Test Title"
         description="Test Description"
-        isHome={false}
+        hasInfoButton={false}
+        isInfoPage={false}
+      />
+    );
+    expect(screen.queryByRole("button")).not.toBeInTheDocument();
+  });
+
+  it("renders the InfoButton when hasInfoButton is true", () => {
+    render(
+      <Header
+        title="Test Title"
+        description="Test Description"
+        hasInfoButton={true}
+        isInfoPage={false}
       />
     );
     expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("renders the back button when isInfoPage is true", () => {
+    render(
+      <Header
+        title="Test Title"
+        description="Test Description"
+        hasInfoButton={false}
+        isInfoPage={true}
+      />
+    );
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("calls router.back when the back button is clicked", () => {
+    render(
+      <Header
+        title="Test Title"
+        description="Test Description"
+        hasInfoButton={false}
+        isInfoPage={true}
+      />
+    );
+    const backButton = screen.getByRole("button");
+    fireEvent.click(backButton);
+    expect(mockedBack).toHaveBeenCalled();
   });
 });
