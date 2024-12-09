0270bbb03cbd862b4f5dcb50da31d01f653668f8
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-09 12:37:43 +0000

    feat: modal can now take an input

--- src/ui/shared/Modal.tsx
+++ src/ui/shared/Modal.tsx
@@ -1,6 +1,8 @@
 import Button from "./Button";
 
 interface ModalProps {
+  type?: string;
+  placeholder?: string;
   modalOpen: boolean;
   title: string;
   forwardButton?: {
@@ -14,6 +16,8 @@ interface ModalProps {
 }
 
 export default function Modal({
+  type,
+  placeholder,
   modalOpen,
   title,
   forwardButton,
@@ -24,7 +28,15 @@ export default function Modal({
       {modalOpen && (
         <div className="h-64 w-11/12 absolute top-1/2 left-1/2 bg-gray-800 border-[1.5px] rounded-lg -translate-x-1/2 -translate-y-1/2">
           <div className="flex flex-col w-full items-center py-10 justify-between h-full">
-            <h2 className="text-2xl w-10/12 text-center">{title}</h2>
+            {type === "input" ? (
+              <input
+                type="text"
+                placeholder={placeholder}
+                className="text-black p-2 mt-5 w-9/12"
+              />
+            ) : (
+              <h2 className="text-2xl w-10/12 text-center">{title}</h2>
+            )}
             <div className="flex justify-center gap-10 w-2/3">
               {backButton && (
                 <Button
