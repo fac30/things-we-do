4fd43532f31903ab8f654c72111d935b1c25c64a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-09 15:16:27 +0000

    refactor: use dynamic from nextjs

--- src/app/moods/page.tsx
+++ src/app/moods/page.tsx
@@ -1,8 +1,13 @@
 import { Header } from "../../ui/shared/Header";
 // import MoodsDisplay from "./components/MoodsDisplay";
-import { Suspense, lazy } from "react";
+// import { Suspense, lazy } from "react";
+import dynamic from "next/dynamic";
 
-const MoodsDisplay = lazy(() => import("./components/MoodsDisplay"));
+const MoodsDisplay = dynamic(() => import("./components/MoodsDisplay"), {
+  ssr: false, // Disables SSR for this component
+});
+
+// const MoodsDisplay = lazy(() => import("./components/MoodsDisplay"));
 
 export default function MoodsPage() {
   return (
@@ -12,9 +17,9 @@ export default function MoodsPage() {
         description="assess your mood before making a decision."
         hasInfoButton={true}
       />
-      <Suspense fallback={<div>Loading moods...</div>}>
-        <MoodsDisplay />
-      </Suspense>
+      {/* <Suspense fallback={<div>Loading moods...</div>}> */}
+      <MoodsDisplay />
+      {/* </Suspense> */}
     </div>
   );
 }
