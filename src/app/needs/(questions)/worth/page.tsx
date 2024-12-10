513602ed8f653c636f546d207aff15632df1445c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-10 17:21:18 +0000

    refactor: use question page"

--- src/app/needs/(questions)/worth/page.tsx
+++
@@ -1,9 +0,0 @@
-import Button from "@/ui/shared/Button";
-
-export default function Worth() {
-  <>
-    <h1>Does it feel worth doing</h1>
-    <Button label="Yes" />
-    <Button label="No" />
-  </>;
-}
