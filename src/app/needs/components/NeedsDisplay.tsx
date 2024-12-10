e9079b38a22f973f16d69955c258e327d132daf0
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-10 15:49:24 +0000

    feat: get data but not working properly

--- src/app/needs/components/NeedsDisplay.tsx
+++ src/app/needs/components/NeedsDisplay.tsx
@@ -1,90 +1,40 @@
+"use client";
+
 import Button from "@/ui/shared/Button";
+import { useState, useEffect } from "react";
 
 export default function NeedsDisplay() {
-  const needsData = [
-    {
-      needName: "Physical Nurturance",
-      needsArr: [
-        "Air",
-        "Food",
-        "Water",
-        "Movement",
-        "Exercise",
-        "Protection (insects, bacteria, virus, predators)",
-        "Rest",
-        "Sexual expression",
-        "Shelter",
-        "Touch",
-      ],
-    },
-    {
-      needName: "Play",
-      needsArr: ["Play", "Laughter"],
-    },
-    {
-      needName: "Interdependence",
-      needsArr: [
-        "Acceptance",
-        "Appreciation",
-        "Closeness",
-        "Community",
-        "Consideration",
-        "Contribution to the enrichment of life",
-        "Emotional safety",
-        "Empathy",
-        "Honesty",
-        "Love",
-        "Reassurance",
-        "Respect",
-        "Support",
-        "Trust",
-        "Understanding",
-        "Warmth",
-      ],
-    },
-    {
-      needName: "Celebration",
-      needsArr: [
-        "Celebration of life fulfilled",
-        "Celebration of dreams fulfilled",
-        "Mourning of dreams unfulfilled",
-        "Mourning losses of loved ones",
-      ],
-    },
-    {
-      needName: "Harmony",
-      needsArr: ["Peace", "Order", "Harmony", "Beauty", "Inspiration"],
-    },
-    {
-      needName: "Integrity",
-      needsArr: ["Authenticity", "Creativity", "Meaning", "Self worth"],
-    },
-    {
-      needName: "Autonomy",
-      needsArr: [
-        "Choose dreams",
-        "Choose goals",
-        "Choose values",
-        "Choose plan to fulfil dreams",
-        "Choose plan to fulfil goals",
-        "Choose plan to fulfil values",
-      ],
-    },
-  ];
+  const [categories, setCategories] = useState([]);
+  const [needs, setNeeds] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch("./mockNeeds.json");
+    const data = await response.json();
+    const { needs_categories, needs, next_actions } = data;
+    setCategories(needs_categories);
+    setNeeds(needs);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
       <div className="w-11/12 m-auto">
-        {needsData.map((need) => {
+        {categories.map((category, index) => {
           return (
             <>
-              <h2 className="text-xl mb-5 font-semibold">{need.needName}</h2>
+              <h2 className="text-xl mb-5 font-semibold" key={index}>
+                {category.name}
+              </h2>
               <div className="flex gap-5 flex-wrap mb-10">
-                {need.needsArr.map((arrItem, index) => {
+                {needs.map((need, index) => {
                   return (
                     <Button
                       key={index}
-                      label={arrItem}
+                      label={need.name}
                       className="bg-gray-600 font-normal text-nowrap"
                     />
                   );
