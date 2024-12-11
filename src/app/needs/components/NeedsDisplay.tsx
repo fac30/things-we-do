88d9f1a8abd7048dfa089d62c82143c7977949a9
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-11 12:29:52 +0000

    feat: display needs better

--- src/app/needs/components/NeedsDisplay.tsx
+++ src/app/needs/components/NeedsDisplay.tsx
@@ -5,12 +5,27 @@ import { useState, useEffect } from "react";
 import { useDatabase } from "@/context/DatabaseContext";
 import retrieveDataObject from "@/lib/utils/retrieveDataObject";
 import { RxDocumentData } from "rxdb";
-import { Insight } from "@/app/insights/components/InsightsDisplay";
+
+// Define the types for the category and need data
+interface Category {
+  id: string;
+  name: string;
+}
+
+interface Need {
+  name: string;
+  category: string; // Assuming category is a reference to the category ID
+}
+
+interface CategorizedNeed {
+  category: string;
+  needs: Need[];
+}
 
 export default function NeedsDisplay() {
   const database = useDatabase();
-  const [categories, setCategories] = useState<RxDocumentData<Insight>[]>([]);
-  const [needs, setNeeds] = useState([]);
+  const [categories, setCategories] = useState<RxDocumentData<Category>[]>([]);
+  const [needs, setNeeds] = useState<RxDocumentData<Need>[]>([]);
 
   const fetchCategories = async () => {
     const response = await database.getFromDb("needs_categories");
@@ -30,26 +45,25 @@ export default function NeedsDisplay() {
     fetchNeeds();
   }, []);
 
-  const categorizedNeeds = categories.map((category) => {
+  // Organize needs by category and store them in an array
+  const categorisedNeeds: CategorizedNeed[] = categories.map((category) => {
     const categoryNeeds = needs.filter((need) => need.category === category.id);
-    return {
-      category: category.name,
-      needs: categoryNeeds,
-    };
+    return { category: category.name, needs: categoryNeeds };
   });
+
+  console.log(categorisedNeeds);
 
   return (
     <>
       <div className="w-11/12 m-auto">
-        {categories.map((category, index) => {
-          const categoryNeeds =
-            categorizedNeeds.find(
-              (categorized) => categorized.category === category.name
-            )?.needs || [];
+        {categorisedNeeds.map((categoryData, index) => {
+          const categoryNeeds = categoryData.needs;
 
           return (
             <div key={index}>
-              <h2 className="text-xl mb-5 font-semibold">{category.name}</h2>
+              <h2 className="text-xl mb-5 font-semibold">
+                {categoryData.category}
+              </h2>
               <div className="flex gap-5 flex-wrap mb-10">
                 {categoryNeeds.map((need, needIndex) => {
                   return (
