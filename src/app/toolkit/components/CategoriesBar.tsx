88a4e328b93bb6bf1386493a10ab20a11cc3112c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-12 14:54:35 +0000

    style: make add category button pretty

--- src/app/toolkit/components/CategoriesBar.tsx
+++ src/app/toolkit/components/CategoriesBar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Button from "@/ui/shared/Button";
 import { useDatabase } from "@/context/DatabaseContext";
 import { useToolkit } from "@/context/ToolkitContext";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 interface Categories {
   id: string;
@@ -15,12 +16,15 @@ interface CategoriesBarProps {
 }
 
 const categoriesBarClass = `
-  whitespace-nowrap flex items-center gap-4 px-4 py-2 
+  whitespace-nowrap flex items-center justify-center gap-4 px-4 py-2 
   overflow-x-auto bg-twd-background border-b 
   border-gray-700 sm:gap-6 sm:px-6  focus:ring-2 focus:ring-twd-secondary-purple
 `;
 
-export default function CategoriesBar({ openModal, refreshCategories }: CategoriesBarProps) {
+export default function CategoriesBar({
+  openModal,
+  refreshCategories,
+}: CategoriesBarProps) {
   const database = useDatabase();
   const [categories, setCategories] = useState<string[]>([]);
   const { selectedCategories, setSelectedCategories } = useToolkit();
@@ -47,15 +51,22 @@ export default function CategoriesBar({ openModal, refreshCategories }: Categori
   };
 
   return (
-    <div
-      className={categoriesBarClass}
-      data-testid="categories-bar"
-    >
+    <div className={categoriesBarClass} data-testid="categories-bar">
       {/* Fixed "+" Button */}
-      <div className="flex-shrink-0">
-        <Button label="+" onClick={openModal} className="bg-twd-primary-purple"/>
+      <div className="flex-shrink-0 justify-center items-center">
+        <button
+          onClick={openModal}
+          className="flex justify-center items-center"
+        >
+          <PlusCircleIcon className="w-8 m-auto " />
+        </button>
+        {/* <Button
+          label="+"
+          onClick={openModal}
+          className="bg-twd-primary-purple"
+        /> */}
       </div>
-  
+
       {/* Scrollable Categories */}
       <div className="flex overflow-x-auto whitespace-nowrap space-x-2 flex-grow">
         <Button
@@ -69,10 +80,10 @@ export default function CategoriesBar({ openModal, refreshCategories }: Categori
           onClick={() => setSelectedCategories([])}
           ariaPressed={selectedCategories.length === 0}
         />
-  
+
         {categories.map((category) => {
           const isActive = selectedCategories.includes(category);
-  
+
           return (
             <Button
               key={category}
