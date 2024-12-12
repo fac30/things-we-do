e24cb8bd34170874a567023072520e8b7ae5ed7c
Jason Warren <contact.jwarren@gmail.com>
2024-12-12 10:43:24 +0000

    feat: :lipstick: make date bar sticky & increase z-index

--- src/app/insights/components/InsightsDisplay.tsx
+++ src/app/insights/components/InsightsDisplay.tsx
@@ -96,7 +96,7 @@ export default function InsightsDisplay() {
 
   return (
     <>
-      <div className="flex text-center w-full m-auto justify-between bg-twd-navbar-background py-2 px-4">
+      <div className="flex text-center w-full m-auto justify-between bg-twd-navbar-background py-2 px-4 sticky top-0 z-50">
         {dateOptions.map((dateOption, index) => {
           const isActive = selectedButton === dateOption;
           return (
