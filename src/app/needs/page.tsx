19d0ad7328eb79210e44fa5d67cbe4a06c1b0afc
maxitect <maxime.downe@gmail.com>
2024-12-12 17:32:42 +0000

    refactor: move header component to needs page.tsx

--- src/app/needs/page.tsx
+++ src/app/needs/page.tsx
@@ -1,8 +1,14 @@
+import { Header } from "@/ui/shared/Header";
 import NeedsDisplay from "./components/NeedsDisplay";
 
 export default function NeedsPage() {
   return (
     <>
+      <Header
+        title="Needs"
+        description="address unmet needs and assess next actions."
+        hasInfoButton={true}
+      />
       <NeedsDisplay />
     </>
   );
