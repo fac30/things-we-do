e8997f93f934961a5f27d29631db07cd92e8d133
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-12 16:52:45 +0000

    chore: add aria-label to floatingbutton

--- src/app/toolkit/components/floatingButton.tsx
+++ src/app/toolkit/components/floatingButton.tsx
@@ -9,7 +9,10 @@ export default function FloatingButton() {
         label="+ Add a Tool"
         className="fixed bottom-24 right-4 bg-twd-primary-purple text-white shadow-lg"
       /> */}
-      <PlusIcon className="w-16 fixed bottom-20 right-5  shadow-lg text-white bg-twd-primary-purple  p-4 rounded-full border-2 border-twd-secondary-purple" />
+      <PlusIcon
+        aria-label="Add New Tool"
+        className="w-16 fixed bottom-20 right-5  shadow-lg text-white bg-twd-primary-purple  p-4 rounded-full border-2 border-twd-secondary-purple"
+      />
     </Link>
   );
 }
