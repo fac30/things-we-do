eb5f4c6ac66b371093eb9d9f7eb13e8758d031be
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-12 14:42:25 +0000

    style: floating button now big and round" "

--- src/app/toolkit/components/floatingButton.tsx
+++ src/app/toolkit/components/floatingButton.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link"; // Import Next.js Link component
 import Button from "@/ui/shared/Button";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 export default function FloatingButton() {
   return (
     <Link href="/toolkit/add-tool">
-      <Button
-        label="+Add a Tool"
+      {/* <Button
+        label="+ Add a Tool"
         className="fixed bottom-24 right-4 bg-twd-primary-purple text-white shadow-lg"
-      />
+      /> */}
+      <PlusIcon className="w-16 fixed bottom-20 right-5  shadow-lg text-white bg-twd-primary-purple  p-4 rounded-full" />
     </Link>
   );
 }
