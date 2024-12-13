4e4522af3c4ca587b227bea48ce608a145e34af0
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-13 16:14:51 +0000

    feat: home page

--- src/app/home/components/HomeContents.tsx
+++ src/app/home/components/HomeContents.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 export default function HomeContents() {
   return (
-    <div className="bg-twd-background text-white min-h-screen px-8 py-4 flex flex-col justify-center text-center">
+    <div className=" text-white min-h-screen px-8 py-4 flex flex-col justify-center text-center">
       <h2 className="text-2xl text-center font-semibold mb-4">Hello There!</h2>
       <p className="text-lg font-thin mb-1">
         Gain awareness and autonomy over what you are feeling and doing:
@@ -18,7 +18,7 @@ export default function HomeContents() {
       {/* Decision maker block */}
       <div className="mt-6">
         <div className="pt-2">
-          <div className="flex items-center space-x-2 mb-4">
+          <div className="flex items-center justify-center space-x-2 mb-4 text-center w-full">
             <PresentationChartLineIcon className="w-5 h-5" />
             <h2 className="text-xl mb-0">Decision maker</h2>
           </div>
@@ -27,16 +27,6 @@ export default function HomeContents() {
             Work out how your decision might affect how you feel, find out if
             it&apos;s important, and what you can do about it.
           </p>
-          <div className="flex justify-center w-full">
-            <Image
-              src="/drawings/maker.svg"
-              alt="Decision maker drawing"
-              width={32}
-              height={32}
-              objectFit="contain"
-              priority
-            />
-          </div>
         </div>
 
         <>
@@ -70,28 +60,27 @@ export default function HomeContents() {
       </div>
 
       {/* Toolkit block */}
-      <div className="mt-6">
-        <div className="pt-2">
+      <section className="mt-6">
+        {/* <div className="pt-2">
           <h2 className="text-xl mb-4">Toolkit</h2>
           <p className="text-base mb-1">
             Create a Toolkit of what helps when you are overwhelmed:
           </p>
-        </div>
+        </div> */}
 
-        <div className="grid grid-cols-2 gap-8 max-w-6xl w-full">
-          <div className="flex flex-col h-full justify-center relative w-full">
-            <div className="flex justify-center">
-              <WrenchIcon className="w-5 h-5" />
-            </div>
+        <div
+          id="column-container-toolkit"
+          className="flex gap-8 max-w-6xl w-full justify-center items-center"
+        >
+          <div className="flex flex-1 flex-col h-full justify-center relative w-full items-center gap-5">
+            <WrenchIcon className="w-10 h-10" />
 
-            <div className="text">
-              <p className="text-base text-center">
-                Create a <span className="font-bold">Toolkit</span> of things
-                that help
-              </p>
-            </div>
+            <p className="text-base text-center">
+              Create a <span className="font-bold">Toolkit</span> of things that
+              help
+            </p>
 
-            <div className="h-1/4 flex items-start">
+            <div className=" flex items-start">
               <Link href="/toolkit">
                 <button className="bg-twd-primary-purple text-white py-2 px-4 rounded-full flex items-center gap-2">
                   <span className="text-base font-medium text-center">
@@ -103,85 +92,63 @@ export default function HomeContents() {
             </div>
           </div>
 
-          <div className="grid grid-rows-2 gap-6">
-            <div className="flex justify-center">
-              <Image
-                src="/drawings/toolkit.svg"
-                alt="Toolkit drawing"
-                width={80}
-                height={80}
-                objectFit="contain"
-                priority
-              />
-            </div>
-
-            <div className="flex justify-center">
-              <Image
-                src="/images/toolkit_screen.png"
-                alt="Toolkit screenshot"
-                className="rounded-lg shadow-md"
-                width={213}
-                height={320}
-                objectFit="contain"
-                priority
-              />
-            </div>
+          <div className="flex justify-center flex-1">
+            <Image
+              src="/images/toolkit_screen.png"
+              alt="Toolkit screenshot"
+              className="rounded-lg shadow-md"
+              width={213}
+              height={320}
+              objectFit="contain"
+              priority
+            />
           </div>
         </div>
-      </div>
+      </section>
 
       {/* Needs block */}
-      <div className="mt-6">
-        <div className="pt-2">
-          <h2 className="text-xl mb-4 text-center">Needs</h2>
-          <p className="text-base mb-1 text-center">
-            Improve your mood by identifying what you need and how to get it:
-          </p>
-          <div className="bg-twd-secondary-purple mx-auto mt-4 mb-4 py-4 w-3/4 h-3/4 flex flex-col items-center justify-center rounded-lg">
+      <section className="mt-6">
+        <div className="pt-2 flex gap-8">
+          <Image
+            src="/images/needsImage.png"
+            alt="Needs screenshot"
+            className="rounded-lg shadow-md flex-1"
+            width={170}
+            height={0}
+            // objectFit="contain"
+            layout="intrinsic"
+            priority
+          />
+          <div className=" mx-auto mt-4 mb-4 py-4 w-1/2 flex flex-col gap-8 items-center justify-center rounded-lg flex-1">
             <EllipsisHorizontalCircleIcon className="w-8 h-8 mb-4" />
-
-            <p className="text-base font-medium mb-4 text-center">
-              What do I need right now?
+            <p className="text-base mb-1 text-center">
+              Improve your mood by identifying what you need and how to get it:
             </p>
 
             <Link href="/needs">
               <button className="bg-twd-primary-purple text-white py-2 px-6 rounded-full flex items-center gap-2">
-                <span className="text-sm font-medium text-center">
+                <span className="text-sm font-medium text-center text-nowrap">
                   Go to Needs
                 </span>
                 <ArrowRightIcon className="w-4 h-4" />
               </button>
             </Link>
           </div>
-          <div className="flex justify-center mb-4">
-            <Image
-              src="/drawings/needs.svg"
-              alt="Needs drawing"
-              width={80}
-              height={80}
-              objectFit="contain"
-              priority
-            />
-          </div>
         </div>
-      </div>
+      </section>
 
       {/* Insights block */}
-      <div className="mt-6">
+      <section className="mt-6">
         <div className="pt-2">
-          <h2 className="text-xl mb-4 text-center">Insights</h2>
+          <div className="flex text-center justify-center gap-2">
+            <ChartBarIcon className=" w-6 h-6 text-white" />
+            <h2 className="text-xl mb-4 text-center">Insights</h2>
+          </div>
           <p className="text-base mb-1 text-center">
             Use the data you collect to explore patterns and trends:
           </p>
         </div>
         <div className="relative">
-          <ChartBarIcon
-            className="absolute w-6 h-6 text-white"
-            style={{
-              top: "10%",
-              left: "50%",
-            }}
-          />
           <div className="w-full flex justify-center">
             <Image
               src="/images/insights_screen.png"
@@ -207,18 +174,7 @@ export default function HomeContents() {
             </button>
           </Link>
         </div>
-
-        <div className="flex justify-end">
-          <Image
-            src="/drawings/insights.svg"
-            alt="Insights drawing"
-            width={140}
-            height={140}
-            objectFit="contain"
-            priority
-          />
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
