2a0ea87d5f73b8f570115ee0d7d9b2e3416777b1
Jason Warren <contact.jwarren@gmail.com>
2024-12-13 17:03:54 +0000

    refactor: :art: WHITESPACE MATE

--- src/app/needs/page.tsx
+++ src/app/needs/page.tsx
@@ -9,12 +9,15 @@ export default function NeedsPage() {
         description="address unmet needs and assess next actions."
         hasInfoButton={true}
       />
+
       <h2 className="text-2xl w-11/12 mb-6 mt-4 m-auto">
         What do you need right now?
       </h2>
+
       <p className="w-11/12 m-auto mb-5">
         Select what you need from the list below
       </p>
+      
       <NeedsDisplay />
     </>
   );
