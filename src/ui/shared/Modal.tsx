d63525c9a0796a4d857006a67b471b611a7597ca
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-15 10:34:36 +0000

    style: change modal input styling

--- src/ui/shared/Modal.tsx
+++ src/ui/shared/Modal.tsx
@@ -41,7 +41,7 @@ export default function Modal({
                 <input
                   type="text"
                   placeholder={placeholder}
-                  className="w-9/12 px-3 py-2 mt-4 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  className="w-9/12 px-3 py-2 mt-4 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-twd-primary-purple addToolInput"
                   onChange={handleInputChange}
                 />
               ) : (
