310d8ccba78be160682ed2a6c1585a1b4da4359a
Jason Warren <contact.jwarren@gmail.com>
2024-12-15 17:54:40 +0000

    feat: :construction: create basic structure for NeedsDisplay

---
+++ src/app/needs/next-actions/components/NextActionsDisplay.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+export default function NextActionsDisplay() {
+  return (
+    <div>
+      <h2 className="text-2xl w-11/12 mb-6 mt-4 m-auto">
+        Next Actions Display (Placeholder)
+      </h2>
+      
+      <p className="w-11/12 m-auto mb-5">
+        This is where we will fetch and display next actions related to highlighted needs.
+      </p>
+    </div>
+  );
+}
