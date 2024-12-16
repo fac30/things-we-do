2e466eafa2412194365a91929f693903733ff7ff
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-16 11:37:24 +0000

    chore: delete unusued imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Header } from "@/ui/shared/Header";
 import HomeContent from "./home/components/HomeContent";
-// import DownloadButton from "./home/components/DownloadButton";
 
 export default function Page() {
   return (
     <>
       <Header title="Home" description="" hasInfoButton={false} />
-      {/* <DownloadButton /> */}
       <HomeContent />
     </>
   );
