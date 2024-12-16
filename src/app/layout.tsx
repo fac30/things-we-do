38b3f98bbdf226c289fa09f2630fb01b3899b1b9
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-16 11:52:10 +0000

    feat: wip placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,8 +60,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" dir="ltr">
       <head />
       <body className={`${poppins.className}`}>
+        <div className="hidden md:absolute md:block top-1/2 left-1/2 h-5/6 w-11/12 overflow-hidden -translate-x-1/2 -translate-y-1/2 bg-twd-primary-purple z-[9999]">
+          <p className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl">
+            fuck off
+          </p>
+        </div>
         <DatabaseProvider>
-          <main className="pb-24">{children}</main>
+          <main className="pb-24 z-[60]">{children}</main>
         </DatabaseProvider>
         <Navbar />
       </body>
