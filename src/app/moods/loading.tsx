2a532e0a4262971bd196ab7960bad7a16ff1614f
maxitect <maxime.downe@gmail.com>
2024-12-17 15:30:41 +0000

    chore: apply more semantic html to loading.tsx by using p element instead of div

--- src/app/moods/loading.tsx
+++ src/app/moods/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div className="text-3xl">Loading...</div>
+      <p className="text-3xl">Loading...</p>
     </div>
   );
 }
