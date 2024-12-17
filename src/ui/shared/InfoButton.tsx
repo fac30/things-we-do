fcf1903d30ab9f714ece36a59cf45e983deb8d91
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-12-17 11:11:14 +0000

    fix: change info icon to the svg info icon- for iphones

--- src/ui/shared/InfoButton.tsx
+++ src/ui/shared/InfoButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
+//import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import Button from "./Button";
 
 interface QuestionMarkButtonProps {
@@ -49,7 +49,11 @@ export default function InfoButton({
         onClick={togglePopup}
         className="flex items-center justify-center w-8 h-8 border-2 border-twd-background rounded-full text-white font-bold transition"
       >
-        <InformationCircleIcon />
+        {/* <InformationCircleIcon className="text-white !important" /> */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+        </svg>
+
       </button>
       {isPopupOpen && (
         <div
