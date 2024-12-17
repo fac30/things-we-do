83e0c8783954b2f78e1846031a96c17809ca9cbf
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-17 14:41:55 +0000

    style: next actions

--- src/app/needs/next-actions/components/NextActionsSection.tsx
+++ src/app/needs/next-actions/components/NextActionsSection.tsx
@@ -24,51 +24,58 @@ export default function NextActionsSection({
   const [newAction, setNewAction] = useState<string>("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewAction(e.target.value)
+    setNewAction(e.target.value);
   };
 
   const handleSubmitAction = async () => {
     await handleAddAction(newAction, need);
     setModalOpen(false);
-  }
+  };
 
-  return ( <>
-    <div className="ml-4 mb-6">
-      {actions.map((action) => {
-        const highlighted = new Date(action.selectedExpiry) > new Date();
+  return (
+    <>
+      <div className="mb-6 flex gap-5 flex-wrap">
+        {actions.map((action) => {
+          const highlighted = new Date(action.selectedExpiry) > new Date();
 
-        return (<Button key={action.id}
-          label={action.name}
-          className={ highlighted ? 
-            "bg-twd-primary-purple text-black" :
-            "bg-gray-600 text-white"
-          }
-          onClick={() => onToggleAction(action)}
-        />);
-      })}
+          return (
+            <Button
+              key={action.id}
+              label={action.name}
+              className={
+                highlighted
+                  ? "bg-twd-primary-purple text-black font-normal"
+                  : "bg-gray-600 text-white font-normal"
+              }
+              onClick={() => onToggleAction(action)}
+            />
+          );
+        })}
 
-      <button onClick={() => setModalOpen(true)}
-        className="flex justify-center items-center"
-        aria-label="Add Action"
-      >
-        <PlusCircleIcon className="w-7 m-auto" />
-      </button>
-    </div>
+        <button
+          onClick={() => setModalOpen(true)}
+          className="flex justify-center items-center"
+          aria-label="Add Action"
+        >
+          <PlusCircleIcon className="w-7 m-auto" />
+        </button>
+      </div>
 
-    <Modal title="Add a New Action"
-      inputModal={true}
-      modalOpen={modalOpen}
-      placeholder="What action might help meet this need?"
-      handleInputChange={handleInputChange}
-      forwardButton={{ label: "Add",
-        action: handleSubmitAction,
-      }}
-      backButton={{ label: "Cancel",
-        action: () => {
-          setNewAction("");
-          setModalOpen(false);
-        },
-      }}
-    />
-  </> );
+      <Modal
+        title="Add a New Action"
+        inputModal={true}
+        modalOpen={modalOpen}
+        placeholder="What action might help meet this need?"
+        handleInputChange={handleInputChange}
+        forwardButton={{ label: "Add", action: handleSubmitAction }}
+        backButton={{
+          label: "Cancel",
+          action: () => {
+            setNewAction("");
+            setModalOpen(false);
+          },
+        }}
+      />
+    </>
+  );
 }
