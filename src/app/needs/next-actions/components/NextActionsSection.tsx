2e81953a3bf70ef02e07f9650c36e5a76a62e987
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-18 12:27:12 +0000

    style: next actions text always white

--- src/app/needs/next-actions/components/NextActionsSection.tsx
+++ src/app/needs/next-actions/components/NextActionsSection.tsx
@@ -22,7 +22,7 @@ export default function NextActionsSection({
   onToggleAction,
   handleAddAction,
   onDeleteAction,
-  mode
+  mode,
 }: SectionProps) {
   const [modalOpen, setModalOpen] = useState(false);
   const [newAction, setNewAction] = useState<string>("");
@@ -38,60 +38,64 @@ export default function NextActionsSection({
 
   return (
     <>
-      { actions.length > 0
-        ? <div className="mb-6 flex gap-5 flex-wrap">
+      {actions.length > 0 ? (
+        <div className="mb-6 flex gap-5 flex-wrap">
           {actions.map((action) => {
             const highlighted = new Date(action.selectedExpiry) > new Date();
 
             return (
-              <Button label={action.name}
+              <Button
+                label={action.name}
                 key={action.id}
-                className={ mode === "destroy"
-                  ? "bg-twd-cube-red text-black font-normal"
-                  : highlighted
-                    ? "bg-twd-primary-purple text-black font-normal"
+                className={
+                  mode === "destroy"
+                    ? "bg-twd-cube-red font-normal"
+                    : highlighted
+                    ? "bg-twd-primary-purple font-normal"
                     : "bg-gray-600 text-white font-normal"
                 }
-                onClick={() => (mode === "destroy"
-                  ? onDeleteAction(action)
-                  : onToggleAction(action)
-                )}
+                onClick={() =>
+                  mode === "destroy"
+                    ? onDeleteAction(action)
+                    : onToggleAction(action)
+                }
               />
             );
           })}
 
-          <button aria-label="Add Action"
+          <button
+            aria-label="Add Action"
             onClick={() => setModalOpen(true)}
             className="flex justify-center items-center"
           >
-            <PlusCircleIcon className="w-7 m-auto"/>
+            <PlusCircleIcon className="w-7 m-auto" />
           </button>
         </div>
-        : (
-          <div className="flex gap-5 flex-wrap items-center">
-            <p className="text-sm text-gray-500 ml-6">
-              No next actions available for this need.
-            </p>
+      ) : (
+        <div className="flex gap-5 flex-wrap items-center">
+          <p className="text-sm text-gray-500 ml-6">
+            No next actions available for this need.
+          </p>
 
-            <button aria-label="Add Action"
-              onClick={() => setModalOpen(true)}
-              className="flex justify-center items-center"
-            >
-              <PlusCircleIcon className="w-7 m-auto"/>
-            </button>
-          </div>
-        )
-      }
+          <button
+            aria-label="Add Action"
+            onClick={() => setModalOpen(true)}
+            className="flex justify-center items-center"
+          >
+            <PlusCircleIcon className="w-7 m-auto" />
+          </button>
+        </div>
+      )}
 
-      <Modal title="Add a New Action"
-        inputModal={ true }
-        modalOpen={ modalOpen }
+      <Modal
+        title="Add a New Action"
+        inputModal={true}
+        modalOpen={modalOpen}
         placeholder="What action might help meet this need?"
-        handleInputChange={ handleInputChange }
-        forwardButton={{ label: "Add",
-          action: handleSubmitAction
-        }}
-        backButton={{ label: "Cancel",
+        handleInputChange={handleInputChange}
+        forwardButton={{ label: "Add", action: handleSubmitAction }}
+        backButton={{
+          label: "Cancel",
           action: () => {
             setNewAction("");
             setModalOpen(false);
