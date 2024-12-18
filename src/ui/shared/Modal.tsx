44fd9492ebc81482699dfebcb1832b40e58923b6
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-18 07:04:01 +0000

    style: add aninations to modal

--- src/ui/shared/Modal.tsx
+++ src/ui/shared/Modal.tsx
@@ -33,40 +33,46 @@ export default function Modal({
 
   return (
     <>
-      {modalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-          <div className="w-11/12 max-w-lg bg-gray-900 border border-gray-700 rounded-lg p-6 shadow-lg">
-            <div className="flex flex-col items-center">
-              {inputModal ? (
-                <input
-                  type="text"
-                  placeholder={placeholder}
-                  className="w-9/12 px-3 py-2 mt-4 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-twd-primary-purple addToolInput"
-                  onChange={handleInputChange}
+      <div
+        className={`fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75 transition-opacity duration-300 ${
+          modalOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <div
+          className={`w-11/12 max-w-lg bg-gray-900 border border-gray-700 rounded-lg p-6 shadow-lg transform transition-transform duration-300 ${
+            modalOpen ? "scale-100" : "scale-95"
+          }`}
+        >
+          <div className="flex flex-col items-center">
+            {inputModal ? (
+              <input
+                type="text"
+                placeholder={placeholder}
+                className="w-9/12 px-3 py-2 mt-4 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-twd-primary-purple addToolInput"
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="mt-4 text-xl text-center text-white">{title}</p>
+            )}
+            <div className="flex justify-center gap-4 mt-6 w-full">
+              {backButton && (
+                <Button
+                  onClick={backButton.action}
+                  label={backButton.label}
+                  className="px-4 py-2 text-lg font-medium text-white bg-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-400 w-36 text-nowrap"
                 />
-              ) : (
-                <p className="mt-4 text-xl text-center text-white">{title}</p>
               )}
-              <div className="flex justify-center gap-4 mt-6 w-full">
-                {backButton && (
-                  <Button
-                    onClick={backButton.action}
-                    label={backButton.label}
-                    className="px-4 py-2 text-lg font-medium text-white bg-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-400 w-36 text-nowrap"
-                  />
-                )}
-                {forwardButton && (
-                  <Button
-                    onClick={forwardButton.action}
-                    label={forwardButton.label}
-                    className="px-4 py-2 text-lg font-medium text-white bg-twd-primary-purple rounded-full focus:outline-none focus:ring-2 focus:ring-purple-400 w-36 text-nowrap"
-                  />
-                )}
-              </div>
+              {forwardButton && (
+                <Button
+                  onClick={forwardButton.action}
+                  label={forwardButton.label}
+                  className="px-4 py-2 text-lg font-medium text-white bg-twd-primary-purple rounded-full focus:outline-none focus:ring-2 focus:ring-purple-400 w-36 text-nowrap"
+                />
+              )}
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
